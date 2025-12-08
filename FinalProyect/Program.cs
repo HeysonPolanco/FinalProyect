66bfa403f06dda9f97170b0acdffb66919466c6e
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-12-08 13:14:54 -0400

    refactor(program.cs): reorganized DI setup, removed obsolete services (Topbar/SignInManager), and updated Identity/Authentication configuration.

--- FinalProyect/Program.cs
+++ FinalProyect/Program.cs
@@ -34,14 +34,9 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddIdentityCore<ApplicationUser>(options =>
     {
-        options.SignIn.RequireConfirmedAccount = true;
+        options.SignIn.RequireConfirmedAccount = false;
         options.Stores.SchemaVersion = IdentitySchemaVersions.Version3;
     })
     .AddEntityFrameworkStores<ApplicationDbContext>()
